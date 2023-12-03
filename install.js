9241b0d3e8ce5087bf2d41ff3710741fcc67a1db
Luis Blanco <luisblanco1337@gmail.com>
2023-12-04 00:08:21 +0400

    Fix repo reference

--- install.js
+++ install.js
@@ -3,7 +3,7 @@
 const { install } = require('addon-tools-raub');
 
 
-const prefix = 'https://github.com/luis-si/iohook-raub/releases/download';
+const prefix = 'https://github.com/node-3d/iohook-raub/releases/download';
 const tag = '0.0.1';
 
 install(`${prefix}/${tag}`);
